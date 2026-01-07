90c531f083eda17a3539e6cc0c91313de7fb9152
FenCra <bezerrafelipecarvalho@gmail.com>
2026-01-07 17:23:32 -0300

    Design: Crud e Visualização única

--- ProjetoIBBAF-angular/src/app/visualizacao-unica/visualizacao-unica.ts
+++ ProjetoIBBAF-angular/src/app/visualizacao-unica/visualizacao-unica.ts
@@ -14,6 +14,7 @@ import { Header } from '../header/header';
 })
 export class VisualizacaoUnica implements OnInit {
   musica: any;
+  favorite: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
